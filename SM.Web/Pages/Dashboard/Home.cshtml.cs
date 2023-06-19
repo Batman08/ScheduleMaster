2ec590add8d29f342395a6aabb022bea05fa69f8
bilal asghar <asgharbilal8@gmail.com>
2023-06-19 21:49:57 +0100

    added schedule home page

---
+++ SM.Web/Pages/Dashboard/Home.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SM.Web.Pages.Dashboard
+{
+    public class HomeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
