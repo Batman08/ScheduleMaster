2ab32465c3f1f85d52ea5a8e774a99203b445272
bilal asghar <asgharbilal8@gmail.com>
2023-06-20 19:02:14 +0100

    Trying Entity Framework

---
+++ SM.Database/SMContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SM.Engine.Dashboard;
+
+namespace SM.Database
+{
+    public class SMContext : DbContext
+    {
+        public SMContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<SmEventDataDTO>().HasNoKey();
+        }
+
+        public DbSet<SmEventDataDTO> SaveEventData { get; set; }
+    }
+}
---
+++ SM.Database/SMRepository.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SM.Engine.Dashboard;
+
+namespace SM.Database
+{
+    public interface ISmRepository
+    {
+        public SmEventDataDTO spSaveUserEvent(SmEventDataDTO smData);
+    }
+
+    public class SMRepository : ISmRepository
+    {
+        private readonly SMContext _smContext;
+
+        public SMRepository(SMContext context)
+        {
+            _smContext = context;
+        }
+
+        public SmEventDataDTO spSaveUserEvent(SmEventDataDTO smData)
+        {
+            //SqlParameter p_UserId = new SqlParameter("@param", smData.UserId);
+            //SqlParameter p_Title = new SqlParameter("@param", smData.Title);
+            //SqlParameter p_Info = new SqlParameter("@param", smData.Info);
+            //SqlParameter p_Start = new SqlParameter("@param", smData.Start);
+            //SqlParameter p_End = new SqlParameter("@param", smData.End);
+            //SqlParameter p_Colour = new SqlParameter("@param", smData.Colour);
+
+            return _smContext.SaveEventData.FromSqlRaw("EXECUTE dbo.spGetMedicalPersons", smData).AsEnumerable().First();
+        }
+    }
+}
