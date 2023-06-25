162e1319b779b85e6a144c2e3c3e4efc5928fd00
bilal asghar <asgharbilal8@gmail.com>
2023-06-25 01:34:10 +0100

    added function to calculate text contrast

--- SM.Web/wwwroot/typescript/Utilities.ts
+++ SM.Web/wwwroot/typescript/Utilities.ts
@@ -7,6 +7,14 @@
         xhr.send(JSON.stringify(body));
     }
 
+    public static GetTextColourContrast(hexcolor) {
+        var r = parseInt(hexcolor.substring(1, 3), 16);
+        var g = parseInt(hexcolor.substring(3, 5), 16);
+        var b = parseInt(hexcolor.substring(5, 7), 16);
+        var yiq = ((r * 299) + (g * 587) + (b * 114)) / 1000;
+        return (yiq >= 128) ? 'black' : 'white';
+    }
+
     public static SetCookie(cname, value, exdays) {
         var exdate = new Date();
         exdate.setDate(exdate.getDate() + exdays);
