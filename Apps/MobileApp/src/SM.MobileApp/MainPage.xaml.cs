1380668c72901d6ec735f2b2a1b682fc50c53d09
bilal asghar <asgharbilal8@gmail.com>
2023-07-02 01:09:44 +0100

    added code to create app wrapper and inject mobile marker into page

--- Apps/MobileApp/src/SM.MobileApp/MainPage.xaml.cs
+++ Apps/MobileApp/src/SM.MobileApp/MainPage.xaml.cs
@@ -2,9 +2,28 @@
 {
     public partial class MainPage : ContentPage
     {
+        private WebView _webView = new WebView();
+
+        private readonly string _url = "https://schedulemaster.bsite.net/";
+
         public MainPage()
         {
-            InitializeComponent();
+            InitWebView();
         }
+
+        private void InitWebView()
+        {
+            _webView.Source = _url;
+            _webView.Navigated += OnWebViewNavigated;
+            Content = _webView;
+        }
+
+        private void OnWebViewNavigated(object sender, WebNavigatedEventArgs e)
+        {
+            /*create mobile marker if it doesn't exist*/
+            //_webView.Eval(Javascript_MobileApp_CreateMarker()); -- todo: create Utilities function
+        }
+
+        private string Javascript_MobileApp_CreateMarker() => "Utilities.MobileApp_CreateMarker();";
     }
 }
