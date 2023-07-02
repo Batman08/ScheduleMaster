c9e4143d9b2936684eee38f8990c312ff427ed33
bilal asghar <asgharbilal8@gmail.com>
2023-07-02 01:00:58 +0100

    added mobile app .net maui project

---
+++ Apps/MobileApp/src/SM.MobileApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SM.MobileApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ Apps/MobileApp/src/SM.MobileApp/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SM.MobileApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
