f0932503752c6b61e56c34bbba43df706f55746a
bilal asghar <asgharbilal8@gmail.com>
2023-07-04 21:48:43 +0100

    changed log out page design and redirect user to dashboard if still logged in while trying to access log out page

---
+++ SM.Web/Areas/Auth/Pages/Account/Logout.cshtml.cs
@@ -0,0 +1,46 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+#nullable disable
+
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SM.Web.Areas.Auth.Pages.Account
+{
+    public class LogoutModel : PageModel
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly ILogger<LogoutModel> _logger;
+
+        public LogoutModel(SignInManager<IdentityUser> signInManager, ILogger<LogoutModel> logger)
+        {
+            _signInManager = signInManager;
+            _logger = logger;
+        }
+
+        public IActionResult OnGet()
+        {
+            //check if user is logged in redirect to user dashboard
+            if (User.Identity!.IsAuthenticated && User.Identity != null)
+            {
+                return LocalRedirect(Url.Content("~/Dashboard/Home"));
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPost(string returnUrl = null)
+        {
+            await _signInManager.SignOutAsync();
+            _logger.LogInformation("User logged out.");
+            if (returnUrl != null)
+            {
+                return LocalRedirect(returnUrl);
+            }
+            else
+            {
+                return RedirectToPage();
+            }
+        }
+    }
+}
