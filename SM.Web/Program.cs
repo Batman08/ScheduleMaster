3e9a6e7cd2dea7f4d0453cb9485ab9e52dc4c832
bilal asghar <asgharbilal8@gmail.com>
2024-01-24 11:25:43 +0000

    updated db connection strings

--- SM.Web/Program.cs
+++ SM.Web/Program.cs
@@ -6,7 +6,7 @@ using SM.Web.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("ScheduleMaster") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+var connectionString = builder.Configuration.GetConnectionString("Debug") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
