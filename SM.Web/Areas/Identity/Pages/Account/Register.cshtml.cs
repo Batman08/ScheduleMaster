c622913872544d334098c83428841ae52eb8166d
bilal asghar <asgharbilal8@gmail.com>
2024-01-24 12:02:44 +0000

    removed confirmed password input and fixed submit button loading spinners

--- SM.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ SM.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -55,11 +55,6 @@ namespace SM.Web.Areas.Identity.Pages.Account
             [DataType(DataType.Password)]
             [Display(Name = "Password")]
             public string Password { get; set; }
-
-            [DataType(DataType.Password)]
-            [Display(Name = "Confirm password")]
-            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
-            public string ConfirmPassword { get; set; }
         }
 
 
