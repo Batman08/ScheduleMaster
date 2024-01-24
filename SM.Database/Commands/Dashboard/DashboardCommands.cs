162a286450746498fca1acec2b18a7bb03906d39
bilal asghar <asgharbilal8@gmail.com>
2024-01-24 12:10:39 +0000

    separated commands and queries

---
+++ SM.Database/Commands/Dashboard/DashboardCommands.cs
@@ -0,0 +1,36 @@
+﻿using SM.Database.DashboardCommandsQueries;
+
+namespace SM.Database.Commands.Dashboard
+{
+    public interface IDashboardCommands
+    {
+        public void SpSaveUserEvent(SmEventDataDTO smData);
+        public void SpUpdateUserEvent(SmEventDataUpdateDTO smUpdateData);
+        public List<SpDeleteUserEventReturnModel> SpDeleteUserEvent(SmEventDataIdDTO smData);
+    }
+
+    public class DashboardCommands : IDashboardCommands
+    {
+        private readonly MyDbContext _smContext;
+
+        public DashboardCommands(MyDbContext context)
+        {
+            _smContext = context;
+        }
+
+        public void SpSaveUserEvent(SmEventDataDTO smData)
+        {
+            _smContext.SpSaveUserEvent(smData.EventDataId.ToString(), smData.UserId.ToString(), smData.Day, smData.Title, smData.Info, smData.Start, smData.End, smData.Colour);
+        }
+
+        public void SpUpdateUserEvent(SmEventDataUpdateDTO smUpdateData)
+        {
+            _smContext.SpUpdateUserEvent(smUpdateData.EventDataId.ToString(), smUpdateData.UserId.ToString(), smUpdateData.Day, smUpdateData.Title, smUpdateData.Info, smUpdateData.Start, smUpdateData.End, smUpdateData.Colour);
+        }
+
+        public List<SpDeleteUserEventReturnModel> SpDeleteUserEvent(SmEventDataIdDTO smData)
+        {
+            return _smContext.SpDeleteUserEvent(smData.EventDataId.ToString(), smData.UserId.ToString());
+        }
+    }
+}
---
+++ SM.Database/Queries/Dashboard/DashboardQueries.cs
@@ -0,0 +1,30 @@
+﻿using SM.Database.DashboardCommandsQueries;
+
+namespace SM.Database.Queries.Dashboard
+{
+    public interface IDashboardQueries
+    {
+        public List<SpGetUserEventsReturnModel> SpLoadUserEvents(string UserId);
+        public List<SpGetUserEventReturnModel> SpLoadUserEvent(SmEventDataIdDTO smEventData);
+    }
+
+    public class DashboardQueries : IDashboardQueries
+    {
+        private readonly MyDbContext _smContext;
+
+        public DashboardQueries(MyDbContext context)
+        {
+            _smContext = context;
+        }
+
+        public List<SpGetUserEventsReturnModel> SpLoadUserEvents(string UserId)
+        {
+            return _smContext.SpGetUserEvents(UserId).ToList();
+        }
+
+        public List<SpGetUserEventReturnModel> SpLoadUserEvent(SmEventDataIdDTO smEventData)
+        {
+            return _smContext.SpGetUserEvent(smEventData.EventDataId.ToString(), smEventData.UserId.ToString()).ToList();
+        }
+    }
+}
